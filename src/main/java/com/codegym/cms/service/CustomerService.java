a72abf29d6358926f36ba670df26c558501ff7be
KhanhNguyen206 <nguyen.ngockhanh206@gmail.com>
2019-07-08 10:29:21 +0700

    Customer Spring Data Repository

---
+++ src/main/java/com/codegym/cms/service/CustomerService.java
@@ -0,0 +1,22 @@
+package com.codegym.cms.service;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CustomerService {
+    public Iterable<Customer> findAll();
+
+    public Customer findById(Long id);
+
+    public void save(Customer customer);
+
+    public void remove(Long id);
+
+    Iterable<Customer> findAllByProvince(Province province);
+
+    Page<Customer> findAll(Pageable pageable);
+
+    Page<Customer> findAllByFirstNameContaining(String firstName, Pageable pageable);
+}
